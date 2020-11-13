52a8b9543b0e0e5ac1ebab220955fea858683a37
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2020-11-14 02:03:10 +0500

    gem skeleton

---
+++ lib/rollout_percentage.rb
@@ -0,0 +1,6 @@
+require "rollout_percentage/version"
+
+module RolloutPercentage
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rollout_percentage/version.rb
@@ -0,0 +1,3 @@
+module RolloutPercentage
+  VERSION = "0.1.0"
+end
