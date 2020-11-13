1452fa7ac574b9e4b4ad2fd3fc9cfd0b5a09fb1c
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2020-11-14 02:04:15 +0500

    implement RolloutPercentage class

--- lib/rollout_percentage.rb
+++ lib/rollout_percentage.rb
@@ -2,5 +2,32 @@ require "rollout_percentage/version"
 
 module RolloutPercentage
   class Error < StandardError; end
-  # Your code goes here...
+
+  class RolloutPercentage
+    attr_accessor :feature_flag, :rollout_client
+
+    def initialize(feature_flag, rollout_client: Rails.application.redis_client)
+      self.feature_flag = feature_flag
+      self.rollout_client = rollout_client
+    end
+
+    def perform
+      if enabled?
+        block_given? ? yield : true
+      else
+        false
+      end
+    end
+
+    private
+
+    def enabled?
+      percentage = rollout_client.get(feature_flag)
+      percentage && random_number < percentage.to_i
+    end
+
+    def random_number
+      rand(100)
+    end
+  end
 end
